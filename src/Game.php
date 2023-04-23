64fd88ecac4f06c689cd9802f7be098bad2160cd
Sergey Kudashev <kudashevs@gmail.com>
2023-04-23 20:45:46 +0300

    Refactor Game reorganized constants

--- src/Game.php
+++ src/Game.php
@@ -13,9 +13,10 @@ class Game
     private const LIST_PLAYERS_MESSAGE = 'players: %s';
     private const PLAYER_ALREADY_EXISTS_MESSAGE = '%s: already existing player';
     private const NOT_ENOUGH_PLAYERS_MESSAGE = 'There is no enough participants';
+    private const UNKNOWN_COMMAND_MESSAGE = 'unknown command';
+
     private const ALREADY_STARTED_MESSAGE = 'You cannot add %s. The game has already started.';
     private const MOVE_UNKNOWN_PLAYER_MESSAGE = 'You cannot move %s. The player does not exist.';
-    private const UNKNOWN_COMMAND_MESSAGE = 'unknown command';
 
     private bool $started = false;
 
