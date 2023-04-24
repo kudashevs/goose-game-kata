0731c77fcda01ce4f4ae80581b59dbf3afac4aa9
Sergey Kudashev <kudashevs@gmail.com>
2023-04-24 17:55:53 +0300

    Update Input add a CLI input implementation

---
+++ src/Input/CliInput.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Kudashevs\GooseGameKata\Input;
+
+class CliInput implements InputInterface
+{
+    public function readLine(): string
+    {
+        $input = fgets(STDIN);
+
+        /*
+         * We want the input to be cleared from any new lines.
+         */
+        return preg_replace('/(\r)?\n/', '', $input);
+    }
+}
