18a75192fc51a8db3cbe35d1c85eff25978b2ed3
Sergey Kudashev <kudashevs@gmail.com>
2023-04-24 14:28:56 +0300

    Refactor Game rename the goose single jump message

--- src/Game.php
+++ src/Game.php
@@ -34,7 +34,7 @@ class Game
     private const MOVE_REGISTERED_PLAYER_MESSAGE = '%s rolls %s, %s. %s moves from %s to %s';
     private const OVERLAP_BOUNCE_BACK_MESSAGE = '. %s bounces! Pippo returns to %s';
     private const BRIDGE_JUMP_MESSAGE = '. %s jumps to %s';
-    private const GOOSE_SINGLE_JUMP_MESSAGE = ', The Goose. %s moves again and goes to %s';
+    private const GOOSE_JUMP_MESSAGE = ', The Goose. %s moves again and goes to %s';
     private const HAS_WINNER_MESSAGE = 'We have a winner. The game is over!';
 
     private bool $hasStarted = false;
@@ -177,7 +177,7 @@ class Game
             $player->updatePosition($jumpPosition);
 
             return sprintf(
-                self::MOVE_REGISTERED_PLAYER_MESSAGE . self::GOOSE_SINGLE_JUMP_MESSAGE,
+                self::MOVE_REGISTERED_PLAYER_MESSAGE . self::GOOSE_JUMP_MESSAGE,
                 $name,
                 $dice1,
                 $dice2,
